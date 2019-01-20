b9dc564d2e9134095de9cb01320b400c78c027b1
coder-eremina-olga <44140793+coder-eremina-olga@users.noreply.github.com>
2019-01-20 22:55:48 +0300

    исправления для защиты

--- js/script.js
+++ js/script.js
@@ -1,23 +1,22 @@
 
 
-    var button = document.querySelector(".feedback");
-    var popup = document.querySelector(".modal");
-    var closeOverlay = document.querySelector(".js-feedback-overlay");
-    var closeModalButton = popup.querySelector(".modal-close");
+var button = document.querySelector(".feedback");
+var popup = document.querySelector(".modal");
+var closeOverlay = document.querySelector(".js-feedback-overlay");
+var closeModalButton = popup.querySelector(".modal-close");
 
-    button.addEventListener("click", function (evt) {
-      evt.preventDefault();
-      popup.classList.add("modal-show");
-      closeOverlay.style.display = "block";
-    });
+button.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("modal-show");
+  closeOverlay.style.display = "block";
+});
 
-    closeModalButton.addEventListener("click", function (evt) {
-      popup.classList.remove("modal-show");
-      closeOverlay.style.display = "none";
-    });
+closeModalButton.addEventListener("click", function (evt) {
+  popup.classList.remove("modal-show");
+  closeOverlay.style.display = "none";
+});
 
-    closeOverlay.addEventListener("click", function (evt) {
-      this.style.display = "none";
-      popup.classList.remove("modal-show");
-    });
-
+closeOverlay.addEventListener("click", function (evt) {
+  this.style.display = "none";
+  popup.classList.remove("modal-show");
+});
